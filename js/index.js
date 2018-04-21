1ed537efa2f83b1213f62cc95c993fd321d95c76
noenadies <33671023+noenadies@users.noreply.github.com>
2018-04-21 01:00:16 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -23,3 +23,54 @@ $(document).ready(function() {
     });
 
 });
+
+var vced="";
+var vcel="";
+var vnodep="";
+  var config = {
+    apiKey: "AIzaSyA_L67YUVbfzQbgNLFbOylVyuQ6wF4Df_g",
+    authDomain: "mototaxi-2b9d7.firebaseapp.com",
+    databaseURL: "https://mototaxi-2b9d7.firebaseio.com",
+    projectId: "mototaxi-2b9d7",
+    storageBucket: "mototaxi-2b9d7.appspot.com",
+    messagingSenderId: "424969414739"
+  };
+  firebase.initializeApp(config);
+
+
+
+var general;
+ unavesmensaje();
+function btaccion(){
+      unavesmensaje();
+      vced=document.getElementById("idcedula").value;
+vcel=document.getElementById("idcelular").value;
+      validadcionuser(vcel,vced)
+
+   // alert(vced+" "+ vcel);
+}
+
+function unavesmensaje()
+{ firebase.database().ref().child("taxis").once('value').then(function(snapshot) {
+    var username = (snapshot.val() && snapshot.val().username) || 'Anonymous';
+ 
+   general=snapshot.val();});}
+
+
+var noregistro=true;
+function validadcionuser(cel,ced){
+for(var val in general){
+
+   if(general[val].cel==cel&&general[val].cedula==ced){
+ alert("encontrado login ");
+ 
+ vnodep=val;
+
+ showAndroidToast("1",vnodep,"","");
+ noregistro=false;
+   } 
+}
+if(noregistro){
+alert("no encontrado login ");
+}
+}
